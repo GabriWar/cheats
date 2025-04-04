865b16f4b960d04d577dae6b219a5c2412c3baee
GabriWar <gabrielduarteg02@gmail.com>
2025-04-04 19:33:24 -0300

    fixed lin comp

--- first/libmeminterface/build.sh
+++ first/libmeminterface/build.sh
@@ -1,9 +1,17 @@
-export LD_LIBRARY_PATH=$PWD/libLIN
+# filepath: /home/gabriwar/Desktop/gitthings/cheats/first/libmeminterface/build.sh
+
+# Set the library path
+export LD_LIBRARY_PATH="$PWD/LIBMEMLIN/libLIN:$LD_LIBRARY_PATH"
+
+# Compile the program
 g++ -std=c++20 -I LIBMEMLIN/includeLIN/libmem/ -L LIBMEMLIN/libLIN/ -llibmem -o test test.cpp
 if [ $? -ne 0 ]; then
-    echo "Build failed."
-    exit 1
+	echo "Build failed."
+	exit 1
 fi
+
+# Make the output executable
 chmod +x test
-#invoke passing arguments passed to the script
-./test $@
+
+# Run the program with any arguments passed to the script
+./test "$@"
